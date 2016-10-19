bf8b03fde62038564fa4dc4c1e4feb1fae6fb525
davidsierradz <davidsierradz@gmail.com>
2016-10-19 10:14:31 -0500

    Changes for one monitor setup in work.

--- lightdm/set-correct-monitors.sh
+++ lightdm/set-correct-monitors.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Set HDMI1 monitor to primary in login for LightDM, then put VGA1 right of this.
-mode="$(xrandr -q|grep -A1 "HDMI1 connected"| tail -1 |awk '{ print $1 }')"
+# Set VGA1 monitor to primary in login for LightDM.
+mode="$(xrandr -q|grep -A1 "VGA1 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
-  xrandr --output LVDS1 --off
-  xrandr --output VGA1 --off
-  xrandr --output HDMI1 --primary --mode 1920x1080
-  xrandr --output VGA1 --mode 1920x1080 --right-of HDMI1
+  xrandr --output VGA1 --primary --mode 1440x900
 fi
