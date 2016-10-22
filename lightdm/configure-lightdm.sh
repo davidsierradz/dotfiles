3962f5af0222c6d88be0a146c4f35ef6ef670be8
davidsierradz <davidsierradz@gmail.com>
2016-10-22 09:33:21 -0500

    Revert changes from one monitor setup and merge other commits.

--- lightdm/configure-lightdm.sh
+++ lightdm/configure-lightdm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Write to lightdm.conf file the path to setting the monitor VGA1 as primary.
+# Write to lightdm.conf file the path to setting the monitors in a laptop with two external monitors, HDMI and VGA.
 sed -i 's/#display-setup-script=/display-setup-script=\/home\/neuromante\/dotfiles\/lightdm\/set-correct-monitors.sh/' /etc/lightdm/lightdm.conf
--- lightdm/set-correct-monitors.sh
+++ lightdm/set-correct-monitors.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Set VGA1 monitor to primary in login for LightDM.
-mode="$(xrandr -q|grep -A1 "VGA1 connected"| tail -1 |awk '{ print $1 }')"
+# Set HDMI1 monitor to primary in login for LightDM, then put VGA1 right of this.
+mode="$(xrandr -q|grep -A1 "HDMI1 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
-  xrandr --output VGA1 --primary --mode 1440x900
+  xrandr --output LVDS1 --off
+  xrandr --output VGA1 --off
+  xrandr --output HDMI1 --primary --mode 1920x1080
+  xrandr --output VGA1 --mode 1920x1080 --right-of HDMI1
 fi
