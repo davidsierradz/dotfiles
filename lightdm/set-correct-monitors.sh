41061193510a96a8af2a7e8c5546a0b6b00f7e31
davidsierradz <davidsierradz@gmail.com>
2017-01-03 21:01:58 -0500

    lightdm
    Set correct setup for two monitors desktop.

--- lightdm/set-correct-monitors.sh
+++ lightdm/set-correct-monitors.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Set HDMI1 monitor to primary in login for LightDM, then put VGA1 right of this.
-mode="$(xrandr -q|grep -A1 "HDMI1 connected"| tail -1 |awk '{ print $1 }')"
+# Set HDMI2 monitor to primary in login for LightDM, then put VGA1 right of this.
+mode="$(xrandr -q|grep -A1 "HDMI2 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
-  xrandr --output LVDS1 --off
   xrandr --output VGA1 --off
-  xrandr --output HDMI1 --primary --mode 1920x1080
-  xrandr --output VGA1 --mode 1920x1080 --right-of HDMI1
+  xrandr --output HDMI2 --primary --mode 1920x1080
+  xrandr --output VGA1 --mode 1920x1080 --left-of HDMI2
 fi
