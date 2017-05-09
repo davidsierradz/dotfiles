cc2cd9928a6fc7b24e74970bf913c03bc5a07d18
davidsierradz <davidsierradz@gmail.com>
2017-05-08 21:55:29 -0500

    add one monitor laptop setup

--- lightdm/set-correct-monitors.sh
+++ lightdm/set-correct-monitors.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Three monitor desktop
 # Set HDMI2 monitor to primary in login for LightDM, then put VGA1 left of this and HDMI1 right.
 mode="$(xrandr -q|grep -A1 "HDMI2 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
@@ -9,3 +10,6 @@ if [ -n "$mode" ]; then
   xrandr --output VGA1 --mode 1920x1080 --left-of HDMI2
   xrandr --output HDMI1 --mode 1440x900 --right-of HDMI2
 fi
+
+# One monitor laptop
+# xrandr --output VGA1 --primary --mode 1680x1050
